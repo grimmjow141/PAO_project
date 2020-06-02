d7084d9a141de1caf24f063da611717bd30b7d1a
Berceanu Adrian <48886310+grimmjow141@users.noreply.github.com>
2020-06-02 16:13:02 -0700

    Delete VIP_Ticket.java

--- VIP_Ticket.java
+++
@@ -1,9 +0,0 @@
-package my_package;
-
-public class VIP_Ticket extends Ticket{
-	VIP_Ticket(Movie movie, Client client, OccupiedSeat occupied_seat) {
-		super(movie, client, occupied_seat);
-		price += 0.25 * price; //the price of a VIP ticket is 25% higher
-		key_code += "^VP";     //i also modify the ticket code just for fun!
-	}
-}
